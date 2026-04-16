ff17b0f426273ea19d277189ec4fbdd1a3b7fa61
Sansar Kakkar <sansarkakkar07@gmail.com>
2026-04-16 20:48:05 +0530

    connections

--- src/components/connections.jsx
+++ src/components/connections.jsx
@@ -1,134 +1,134 @@
-// import axios from 'axios';
-// import React, { useEffect } from 'react';
-// import { BASE_URL } from '../utils/constants';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addConnectons } from '../utils/connectionSlice';
-// const Connections = () => {
-//   const connections = useSelector((store) => store.connections);
-//   const dispatch = useDispatch();
-
-//   const fetchConnections = async () => {
-// try{ 
-//     const res= await axios.get(BASE_URL+"/user/connections",{withCredentials:true}); 
-//     dispatch(addConnectons(res.data.data)); 
-// } 
-// catch(err){ 
-
-// }
-//   };
-
-//   useEffect(() => {
-//     fetchConnections();
-//   }, []);
-
-//   if (!connections) return;
-
-//   if (connections.length === 0)
-//     return <h1>No Connections Found</h1>;
-
-//   return (
-//     <div className="text-center my-10">
-//       <h1 className="text-bold text-3xl">Connections</h1>
-
-//       {connections.map((connection) => {
-//         const { firstName, lastName, photoUrl, age, gender, about } =
-//           connection;
-
-//         return (
-//           <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
-//             <div>
-//               <img
-//                 alt="photo"
-//                 className="w-20 h-20 rounded-full"
-//                 src={photoUrl}
-//               />
-//             </div>
-
-//             <div className="text-left mx-4">
-//               <h2 className="font-bold text-xl">
-//                 {firstName + " " + lastName}
-//               </h2>
-
-//               {age && gender && (
-//                 <p>{age + ", " + gender}</p>
-//               )}
-
-//               <p>{about}</p>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// export default Connections;
-import React from 'react';
-import axios from "axios";
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import { BASE_URL } from '../utils/constants';
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addConnections } from '../utils/connectionSlice';
-
 const Connections = () => {
-    const connections = useSelector((store) => store.connections);
-    const dispatch = useDispatch();
+  const connections = useSelector((store) => store.connections);
+  const dispatch = useDispatch();
 
-    const fetchConnections = async() => {
-        try{
-            const res = await axios.get(BASE_URL + "/user/connections", 
-                {withCredentials: true},
-            );
-            dispatch(addConnections(res.data.data));
-        }
-        catch(err){
-            console.log(err.message);
-        }
-    };
+  const fetchConnections = async () => {
+try{ 
+    const res= await axios.get(BASE_URL+"/user/connections",{withCredentials:true}); 
+    dispatch(addConnections(res.data.data)); 
+} 
+catch(err){ 
 
-
-    useEffect(() => {
-        fetchConnections();
-    }, []);
-
-    if (!connections) return <h1>Loading...</h1>;        
-    if (connections.length === 0) return <h1>No Connections Found!</h1>;
-
-    return (
-      <div className="text-center mb-10">
-      <h1 className="text-4xl font-extrabold mb-12 bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-400 
-      bg-clip-text text-transparent inline-block">Connections</h1>
-
-    <div className="flex flex-col items-center gap-6">
-
-      {connections.map(({ firstName, lastName, photoUrl, about, age, gender }, index) => (
-        
-         <div key={index} className="flex items-center bg-gray-900 border border-white rounded-xl p-5 w-[650px] 
-          shadow-md hover:shadow-2xl hover:scale-105 transition-all duration-300 cursor-pointer">
-          
-          <img src={photoUrl} alt="photo" className="w-20 h-20 rounded-full object-cover"/>
-
-          <div className="ml-6 text-left">
-            <h2 className="text-xl font-semibold">
-              {firstName} {lastName}
-            </h2>
-
-            <p className="text-white 700 mt-2">
-              {about}
-            </p>
-
-            <p className="text-white text-sm mt-1">
-              {age} • {gender}
-            </p>
-
-          </div>
-
-        </div>
-      ))}
-
-    </div>
-  </div>
-);
 }
+  };
 
-export default Connections
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+
+  if (!connections) return;
+
+  if (connections.length === 0)
+    return <h1>No Connections Found</h1>;
+
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-3xl">Connections</h1>
+
+      {connections.map((connection) => {
+        const { firstName, lastName, photoUrl, age, gender, about } =
+          connection;
+
+        return (
+          <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
+            </div>
+
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+
+              {age && gender && (
+                <p>{age + ", " + gender}</p>
+              )}
+
+              <p>{about}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Connections;
+// import React from 'react';
+// import axios from "axios";
+// import { BASE_URL } from '../utils/constants';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addConnections } from '../utils/connectionSlice';
+
+// const Connections = () => {
+//     const connections = useSelector((store) => store.connections);
+//     const dispatch = useDispatch();
+
+//     const fetchConnections = async() => {
+//         try{
+//             const res = await axios.get(BASE_URL + "/user/connections", 
+//                 {withCredentials: true},
+//             );
+//             dispatch(addConnections(res.data.data));
+//         }
+//         catch(err){
+//             console.log(err.message);
+//         }
+//     };
+
+
+//     useEffect(() => {
+//         fetchConnections();
+//     }, []);
+
+//     if (!connections) return <h1>Loading...</h1>;        
+//     if (connections.length === 0) return <h1>No Connections Found!</h1>;
+
+//     return (
+//       <div className="text-center mb-10">
+//       <h1 className="text-4xl font-extrabold mb-12 bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-400 
+//       bg-clip-text text-transparent inline-block">Connections</h1>
+
+//     <div className="flex flex-col items-center gap-6">
+
+//       {connections.map(({ firstName, lastName, photoUrl, about, age, gender }, index) => (
+        
+//          <div key={index} className="flex items-center bg-gray-900 border border-white rounded-xl p-5 w-[650px] 
+//           shadow-md hover:shadow-2xl hover:scale-105 transition-all duration-300 cursor-pointer">
+          
+//           <img src={photoUrl} alt="photo" className="w-20 h-20 rounded-full object-cover"/>
+
+//           <div className="ml-6 text-left">
+//             <h2 className="text-xl font-semibold">
+//               {firstName} {lastName}
+//             </h2>
+
+//             <p className="text-white 700 mt-2">
+//               {about}
+//             </p>
+
+//             <p className="text-white text-sm mt-1">
+//               {age} • {gender}
+//             </p>
+
+//           </div>
+
+//         </div>
+//       ))}
+
+//     </div>
+//   </div>
+// );
+// }
+
+// export default Connections
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,101 +1,25 @@
-// import React from 'react';
-// import { useDispatch, useSelector  } from 'react-redux';
-// import { Link, useNavigate } from 'react-router-dom';
-// import { BASE_URL } from '../utils/constants';
-// import axios from 'axios';
-// import { removeUser } from '../utils/userSlice';
-// const Navbar = () => {
-//   const user = useSelector(store=> store.user);
-//   const dispatch=useDispatch();
-//   const Navigate=useNavigate();
-//   const handelLogout=async()=>{
-//     try{
-//       const res=await axios.post(BASE_URL+"/logout",{},{
-//         withCredentials:true
-//       });
-//       dispatch(removeUser());
-//       return Navigate("/login");
-//     }
-//     catch(err){
-
-//     }
-//   }
-//   return (
-//     <div className="navbar bg-neutral text-neutral-content"> 
-
-//         <div className="navbar-start">
-//           <Link to="/" className="btn btn-ghost text-xl">😊 Dev Tinder </Link>
-//        </div>
-
-//     {user && (
-//       <div className="navbar-end flex items-center gap-3">
-
-//       <p className="font-semibold mr-2">
-//         Welcome, {user.firstName}
-//       </p>
-
-//     <div className="dropdown dropdown-end mx-5 flex">
-//       <div tabIndex={-1} role="button" className="btn btn-ghost btn-circle avatar">
-
-//         <div className="w-10 rounded-full">
-//           <img
-//             alt="User photo"
-//             src={user.photoUrl} />
-//         </div>
-//       </div>
-
-
-//       <ul
-//         tabIndex="-1"
-//         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-//         <li>
-//           <Link to='/profile' className="justify-between">
-//             Profile
-//             <span className="badge">New</span>
-//           </Link>
-//         </li>
-//         <li>
-//           <Link to="/connections">connections</Link></li>
-//         <li><a onClick={handelLogout}>Logout</a></li>
-//       </ul>
-//     </div>
-//     </div>
-//     )}
-
-//   </div>
-//   )
-// }
-
-// export default Navbar
 import React from 'react';
-import { useSelector  } from 'react-redux';
+import { useDispatch, useSelector  } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../utils/constants';
-import axios from "axios";
-import { useDispatch } from 'react-redux';
+import axios from 'axios';
 import { removeUser } from '../utils/userSlice';
-
-
 const Navbar = () => {
   const user = useSelector(store=> store.user);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
+  const dispatch=useDispatch();
+  const Navigate=useNavigate();
+  const handelLogout=async()=>{
     try{
-        await axios.post(
-        BASE_URL + "/logout", 
-        {}, 
-        {withCredentials: true}),
-
-        dispatch(removeUser());
-        return navigate("/login");
+      const res=await axios.post(BASE_URL+"/logout",{},{
+        withCredentials:true
+      });
+      dispatch(removeUser());
+      return Navigate("/login");
     }
     catch(err){
-      console.log(err.message);
-    }
-  };
 
+    }
+  }
   return (
     <div className="navbar bg-neutral text-neutral-content"> 
 
@@ -111,7 +35,7 @@ const Navbar = () => {
       </p>
 
     <div className="dropdown dropdown-end mx-5 flex">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+      <div tabIndex={-1} role="button" className="btn btn-ghost btn-circle avatar">
 
         <div className="w-10 rounded-full">
           <img
@@ -122,7 +46,7 @@ const Navbar = () => {
 
 
       <ul
-        tabIndex={0}
+        tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
           <Link to='/profile' className="justify-between">
@@ -131,14 +55,8 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <Link to="/connections">Connections</Link>
-          </li>
-          
-        <li>
-          <Link to="/requests">Requests</Link>
-        </li>
-
-        <li><a onClick={handleLogout}>Logout</a></li>
+          <Link to="/connections">connections</Link></li>
+        <li><a onClick={handelLogout}>Logout</a></li>
       </ul>
     </div>
     </div>
@@ -149,3 +67,85 @@ const Navbar = () => {
 }
 
 export default Navbar
+// import React from 'react';
+// import { useSelector  } from 'react-redux';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { BASE_URL } from '../utils/constants';
+// import axios from "axios";
+// import { useDispatch } from 'react-redux';
+// import { removeUser } from '../utils/userSlice';
+
+
+// const Navbar = () => {
+//   const user = useSelector(store=> store.user);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const handleLogout = async () => {
+//     try{
+//         await axios.post(
+//         BASE_URL + "/logout", 
+//         {}, 
+//         {withCredentials: true}),
+
+//         dispatch(removeUser());
+//         return navigate("/login");
+//     }
+//     catch(err){
+//       console.log(err.message);
+//     }
+//   };
+
+//   return (
+//     <div className="navbar bg-neutral text-neutral-content"> 
+
+//         <div className="navbar-start">
+//           <Link to="/" className="btn btn-ghost text-xl">😊 Dev Tinder </Link>
+//        </div>
+
+//     {user && (
+//       <div className="navbar-end flex items-center gap-3">
+
+//       <p className="font-semibold mr-2">
+//         Welcome, {user.firstName}
+//       </p>
+
+//     <div className="dropdown dropdown-end mx-5 flex">
+//       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+
+//         <div className="w-10 rounded-full">
+//           <img
+//             alt="User photo"
+//             src={user.photoUrl} />
+//         </div>
+//       </div>
+
+
+//       <ul
+//         tabIndex={0}
+//         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+//         <li>
+//           <Link to='/profile' className="justify-between">
+//             Profile
+//             <span className="badge">New</span>
+//           </Link>
+//         </li>
+//         <li>
+//           <Link to="/connections">Connections</Link>
+//           </li>
+          
+//         <li>
+//           <Link to="/requests">Requests</Link>
+//         </li>
+
+//         <li><a onClick={handleLogout}>Logout</a></li>
+//       </ul>
+//     </div>
+//     </div>
+//     )}
+
+//   </div>
+//   )
+// }
+
+// export default Navbar
